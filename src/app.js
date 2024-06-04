5c0fb14217710f22092f527c38290f456207f936
Letimachado <leti.machado.gerez@gmail.com>
2024-06-04 01:16:50 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,62 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = [
+  "Mi perro",
+  "Mi mamá",
+  "El vecino",
+  "La maestra",
+  "Mi tia",
+  "La cajera",
+  "El verdulero"
+];
+let action = [
+  "comió",
+  "pisó",
+  "quebró",
+  "rompió",
+  "perdió",
+  "desapareció",
+  "se llevó"
+];
+let what = [
+  "mi tarea",
+  "mi celular",
+  "nuestro auto",
+  "mi tarjeta",
+  "mis documentos",
+  "mi comida"
+];
+let when = [
+  "en la clase",
+  "mientras yo dormia",
+  "cuando yo estaba en el gimnasio",
+  "mientras yo almorzaba",
+  "cuando yo estaba estudiando",
+  "mientras yo estaba en una reunión"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
